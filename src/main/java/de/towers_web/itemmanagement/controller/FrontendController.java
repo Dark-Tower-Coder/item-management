ce5a4a15395740d7cedcbad1941fb8c000d927e6
Dark-Tower-Coder <tower@posteo.de>
2021-03-28 12:25:24 +0200

    deleted unused files and duplicated files; FrontendController return 404.html if unknown page was called

--- src/main/java/de/towers_web/itemmanagement/controller/FrontendController.java
+++ src/main/java/de/towers_web/itemmanagement/controller/FrontendController.java
@@ -1,29 +1,24 @@
 package de.towers_web.itemmanagement.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FrontendController {
 
-/*    @GetMapping("/index")
-    public String getIndexPage() {
+    private String[] availableSites = new String[]{"401", "404", "500", "charts", "index", "layout-sidenav-light", "layout-static", "login", "password", "register", "tables"};
 
-        return "index";
-    }
-
-    @GetMapping("/tables")
-    public String getTablesPage() {
-
-        return "tables";
-    }*/
 
     @GetMapping(value = {"/{site}"})
     public String site(@PathVariable String site) {
-        return site;
+
+        for (String availableSite : availableSites) {
+            if (availableSite.equals(site)) {
+                return site;
+            }
+        }
+
+        return "404";
     }
 }
